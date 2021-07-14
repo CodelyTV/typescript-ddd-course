f9516b425805d7413c8e8e5664c2deb2d875a132
Alexis Martínez Chacón <alexismartinezchacon@gmail.com>
2021-07-14 19:49:21 +0200

    22-1 use ObjectMother pattern in tests

--- 22-course-aggregate-tests/1-object-mother-pattern/tests/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.test.ts
+++ 22-course-aggregate-tests/1-object-mother-pattern/tests/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.test.ts
@@ -1,17 +1,10 @@
-import { Course } from '../../../../../../src/Contexts/Mooc/Courses/domain/Course';
-import { CourseDuration } from '../../../../../../src/Contexts/Mooc/Courses/domain/CourseDuration';
-import { CourseName } from '../../../../../../src/Contexts/Mooc/Courses/domain/CourseName';
 import { FileCourseRepository } from '../../../../../../src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository';
-import { CourseId } from '../../../../../../src/Contexts/Mooc/Shared/domain/Courses/CourseId';
+import { CourseMother } from '../../domain/CourseMother';
 
 describe('Save Course', () => {
   it('should have a course', async () => {
     const repository = new FileCourseRepository();
-    const course = new Course({
-      id: new CourseId('0766c602-d4d4-48b6-9d50-d3253123275e'),
-      name: new CourseName('name'),
-      duration: new CourseDuration('duration')
-    });
+    const course = CourseMother.random();
 
     await repository.save(course);
   });
