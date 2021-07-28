e729158ef9baedc64c3fe4827401b5b4e4a88504
rsaladocid <rsaladocid@gmail.com>
2021-07-28 22:18:49 +0200

    23-2 mongo repository

--- 23-mongo-persistence/2-mongo-repository/src/Contexts/Mooc/Courses/domain/Course.ts
+++ 23-mongo-persistence/2-mongo-repository/src/Contexts/Mooc/Courses/domain/Course.ts
@@ -1,15 +1,37 @@
+import { AggregateRoot } from '../../../Shared/domain/AggregateRoot';
 import { CourseId } from '../../Shared/domain/Courses/CourseId';
 import { CourseDuration } from './CourseDuration';
 import { CourseName } from './CourseName';
 
-export class Course {
+export class Course extends AggregateRoot {
   readonly id: CourseId;
   readonly name: CourseName;
   readonly duration: CourseDuration;
 
   constructor({ id, name, duration }: { id: CourseId; name: CourseName; duration: CourseDuration }) {
+    super();
     this.id = id;
     this.name = name;
     this.duration = duration;
+  }
+
+  static fromPrimitives(plainData: {
+    id: string;
+    name: string;
+    duration: string;
+  }): Course {
+    return new Course({
+      id: new CourseId(plainData.id),
+      name: new CourseName(plainData.name),
+      duration: new CourseDuration(plainData.duration)
+    });
+  }
+
+  toPrimitives(): any {
+    return {
+      id: this.id.value,
+      name: this.name.value,
+      duration: this.duration.value
+    };
   }
 }
