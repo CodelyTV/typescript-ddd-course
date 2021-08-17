53a33db8779a036071ad0c0bc306026bac4db432
Alexis Martínez Chacón <alexismartinezchacon@gmail.com>
2021-08-17 17:43:53 +0200

    23-3 Initial commit

---
+++ 23-mongo-persistence/3-mongo-integration-test/tests/Contexts/Mooc/Courses/application/CreateCourseRequestMother.ts
@@ -0,0 +1,17 @@
+import { CourseDurationMother } from '../domain/CourseDurationMother';
+import { CourseIdMother } from '../../Shared/domain/Courses/CourseIdMother';
+import { CourseNameMother } from '../domain/CourseNameMother';
+import { CreateCourseRequest } from '../../../../../src/Contexts/Mooc/Courses/application/CreateCourseRequest';
+import { CourseDuration } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseDuration';
+import { CourseName } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseName';
+import { CourseId } from '../../../../../src/Contexts/Mooc/Shared/domain/Courses/CourseId';
+
+export class CreateCourseRequestMother {
+  static create(id: CourseId, name: CourseName, duration: CourseDuration): CreateCourseRequest {
+    return { id: id.value, name: name.value, duration: duration.value };
+  }
+
+  static random(): CreateCourseRequest {
+    return this.create(CourseIdMother.random(), CourseNameMother.random(), CourseDurationMother.random());
+  }
+}
