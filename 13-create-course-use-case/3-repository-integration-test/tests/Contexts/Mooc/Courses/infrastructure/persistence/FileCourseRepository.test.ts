fa9732ff51565f19463b4014a0183a5c7e8de662
Fernando Vilas Maciel <vilasmaciel@gmail.com>
2021-11-22 16:16:39 +0100

    13-3 Add repository implementation and integration test

---
+++ 13-create-course-use-case/3-repository-integration-test/tests/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.test.ts
@@ -0,0 +1,11 @@
+import { Course } from '../../../../../../src/Contexts/Mooc/Courses/domain/Course';
+import { FileCourseRepository } from '../../../../../../src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository';
+
+describe('Save Course', () => {
+  it('should have a course', async () => {
+    const repository = new FileCourseRepository();
+    const course = new Course({ id: 'id', name: 'name', duration: 'duration' });
+
+    await repository.save(course);
+  });
+});
