f527d3edf836975fb397136136375a543c99f0f8
rsaladocid <rsaladocid@gmail.com>
2021-12-03 21:47:14 +0100

    Change mongo config and typeorm transformer

--- 24-postgresql-typeorm-persistence/2-postgres-repository-tests/src/Contexts/Shared/infrastructure/persistence/typeorm/ValueObjectTransformer.ts
+++ 24-postgresql-typeorm-persistence/2-postgres-repository-tests/src/Contexts/Shared/infrastructure/persistence/typeorm/ValueObjectTransformer.ts
@@ -1,9 +1,9 @@
 import { NewableClass } from "../../../domain/NewableClass"
 import { ValueObject } from "../../../domain/value-object/ValueObject"
   
-export const ValueObjectTransformer = (ValueObject: NewableClass<ValueObject<unknown>>) => {
+export const ValueObjectTransformer = (ValueObject: NewableClass<ValueObject<any>>) => {
   return {
-    to: (value: ValueObject<unknown>): unknown => value.value,
-    from: (value: unknown): ValueObject<unknown> => new ValueObject(value)
+    to: (value: ValueObject<any>): any => value.value,
+    from: (value: any): ValueObject<any> => new ValueObject(value)
   }
 }
