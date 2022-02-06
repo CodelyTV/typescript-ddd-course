71cb941fdf68de8827d07878a4f390d4c9b23168
rsaladocid <rsaladocid@gmail.com>
2022-02-06 12:59:03 +0100

    6.1 Using value objects

--- 6-course-aggregate-value-objects-tests/1-value-objects/src/Contexts/Mooc/Courses/domain/Course.ts
+++ 6-course-aggregate-value-objects-tests/1-value-objects/src/Contexts/Mooc/Courses/domain/Course.ts
@@ -1,11 +1,13 @@
 import { CourseId } from '../../Shared/domain/Courses/CourseId';
+import { CourseDuration } from './CourseDuration';
+import { CourseName } from './CourseName';
 
 export class Course {
   readonly id: CourseId;
-  readonly name: string;
-  readonly duration: string;
+  readonly name: CourseName;
+  readonly duration: CourseDuration;
 
-  constructor({ id, name, duration }: { id: CourseId; name: string; duration: string }) {
+  constructor({ id, name, duration }: { id: CourseId; name: CourseName; duration: CourseDuration }) {
     this.id = id;
     this.name = name;
     this.duration = duration;
