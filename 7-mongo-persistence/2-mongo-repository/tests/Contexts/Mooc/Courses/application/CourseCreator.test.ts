2284ebebc95cdca106385f6f2bb7c0d3903f9bff
rsaladocid <rsaladocid@gmail.com>
2022-02-07 10:22:44 +0100

    7.2 Initial commit

---
+++ 7-mongo-persistence/2-mongo-repository/tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
@@ -0,0 +1,37 @@
+import { CourseCreator } from '../../../../../src/Contexts/Mooc/Courses/application/CourseCreator';
+import { CourseMother } from '../domain/CourseMother';
+import { CourseNameLengthExceeded } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseNameLengthExceeded';
+import { CourseRepositoryMock } from '../__mocks__/CourseRepositoryMock';
+import { CreateCourseRequestMother } from './CreateCourseRequestMother';
+
+let repository: CourseRepositoryMock;
+let creator: CourseCreator;
+
+beforeEach(() => {
+  repository = new CourseRepositoryMock();
+  creator = new CourseCreator(repository);
+});
+
+describe('CourseCreator', () => {
+  it('should create a valid course', async () => {
+    const request = CreateCourseRequestMother.random();
+
+    const course = CourseMother.fromRequest(request);
+
+    await creator.run(request);
+
+    repository.assertSaveHaveBeenCalledWith(course);
+  });
+
+  it('should throw error if course name length is exceeded', async () => {
+    expect(() => {
+      const request = CreateCourseRequestMother.invalidRequest();
+
+      const course = CourseMother.fromRequest(request);
+
+      creator.run(request);
+
+      repository.assertSaveHaveBeenCalledWith(course);
+    }).toThrow(CourseNameLengthExceeded);
+  });
+});
