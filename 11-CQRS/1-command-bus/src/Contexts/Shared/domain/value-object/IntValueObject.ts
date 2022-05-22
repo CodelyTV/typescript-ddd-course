c375c13c3102ab05166ab7e306eee38020180d5a
Fernando Vilas Maciel <vilasmaciel@gmail.com>
2022-05-22 19:45:41 +0200

    11.1 Initial commit

---
+++ 11-CQRS/1-command-bus/src/Contexts/Shared/domain/value-object/IntValueObject.ts
@@ -0,0 +1,7 @@
+import { ValueObject } from "./ValueObject";
+
+export abstract class NumberValueObject extends ValueObject<number> {
+  isBiggerThan(other: NumberValueObject): boolean {
+    return this.value > other.value;
+  }
+}
---
+++ 11-CQRS/1-command-bus/src/apps/mooc/backend/MoocBackendApp.ts
@@ -0,0 +1,36 @@
+import { EventBus } from '../../../Contexts/Shared/domain/EventBus';
+import container from './dependency-injection';
+import { DomainEventSubscribers } from '../../../Contexts/Shared/infrastructure/EventBus/DomainEventSubscribers';
+import { Server } from './server';
+import { RabbitMqConnection } from '../../../Contexts/Shared/infrastructure/EventBus/RabbitMQ/RabbitMqConnection';
+
+export class MoocBackendApp {
+  server?: Server;
+
+  async start() {
+    const port = process.env.PORT || '5000';
+    this.server = new Server(port);
+
+    await this.configureEventBus();
+
+    return this.server.listen();
+  }
+
+  get httpServer() {
+    return this.server?.getHTTPServer();
+  }
+
+  async stop() {
+    const rabbitMQConnection = container.get<RabbitMqConnection>('Mooc.Shared.RabbitMQConnection');
+    await rabbitMQConnection.close();
+    return this.server?.stop();
+  }
+
+  private async configureEventBus() {
+    const eventBus = container.get<EventBus>('Mooc.Shared.domain.EventBus');
+    const rabbitMQConnection = container.get<RabbitMqConnection>('Mooc.Shared.RabbitMQConnection');
+    await rabbitMQConnection.connect();
+
+    eventBus.addSubscribers(DomainEventSubscribers.from(container));
+  }
+}
